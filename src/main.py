e68a3a09c0a41e6d48477c6954d3cdfaa25f193a
Whaletrucker Reef <scutua.tua@icloud.com>
2026-05-20 13:13:38 +0000

    fix: allow any accept header

--- src/main.py
+++ src/main.py
@@ -120,6 +120,6 @@ from starlette.responses import JSONResponse
 
 async def main():
     bootstrap()
-    await app.run_http_async(host="0.0.0.0", port=port)
+    await app.run_http_async(host="0.0.0.0", port=port, allow_any_accept=True)
 if __name__ == "__main__":
     asyncio.run(main())
