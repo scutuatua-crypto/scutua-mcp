5118c185ec96bcb40ce1abe8050e6f2aee1e157a
Whaletrucker Reef <scutua.tua@icloud.com>
2026-05-23 15:30:44 +0700

    Update __init__.py

--- tools/market/__init__.py
+++ tools/market/__init__.py
@@ -1,1 +1,14 @@
+from .coingecko import register_coingecko_tools
+from .cmc import register_cmc_tools
+from .trending import register_trending_tools
+from .sentiment import register_sentiment_tools
+from .fear_index import register_fear_index_tools
+
+__all__ = [
+    "register_coingecko_tools",
+    "register_cmc_tools",
+    "register_trending_tools",
+    "register_sentiment_tools",
+    "register_fear_index_tools",
+]
 
