321fc65e1acf1ceb42d2ca8eccb6bf79c78e8949
Whaletrucker Reef <scutua.tua@icloud.com>
2026-05-23 16:32:38 +0700

    Create __init__.py

---
+++ tools/market/__init__.py
@@ -0,0 +1,5 @@
+from src.tools.coingecko import register_coingecko_tools
+from src.tools.cmc import register_cmc_tools
+from src.tools.trending import register_trending_tools
+from src.tools.sentiment import register_sentiment_tools
+from src.tools.fear_index import register_fear_index_tools
