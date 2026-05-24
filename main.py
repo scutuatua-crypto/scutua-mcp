ce804e8974fa602fb0692e13dcd6dfd819c229bb
Whaletrucker Reef <scutua.tua@icloud.com>
2026-05-24 15:12:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import os
-from mcp.server.fastmcp import FastMCP
+from fastmcp import FastMCP
 from src.tools.registry import register_all_tools
 
 app = FastMCP("Scutua-MCP")
-
 register_all_tools(app)
 
 if __name__ == "__main__":
-    import uvicorn
     port = int(os.environ.get("PORT", 10000))
-    uvicorn.run(app.get_asgi_app(), host="0.0.0.0", port=port)
+    app.run(transport="streamable-http", host="0.0.0.0", port=port)
