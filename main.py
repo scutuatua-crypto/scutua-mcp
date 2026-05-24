3d679ab24ce810a18fba390e050645dd952050a7
Whaletrucker Reef <scutua.tua@icloud.com>
2026-05-24 12:46:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import os
 from mcp.server.fastmcp import FastMCP
-from tools.registry import register_all_tools
+from src.tools.registry import register_all_tools
 
 app = FastMCP("Scutua-MCP")
 
 register_all_tools(app)
 
 if __name__ == "__main__":
-    # Render provides the port in an environment variable
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+   port = int(os.environ.get("PORT", 10000))
+   app.run(host="0.0.0.0", port=port)
