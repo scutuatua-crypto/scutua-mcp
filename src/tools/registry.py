a1e0a9b75b01cca27d03eb8c857a9eb05b40eca5
Whaletrucker Reef <scutua.tua@icloud.com>
2026-05-24 09:41:47 +0700

    Create registry.py

---
+++ src/tools/registry.py
@@ -0,0 +1,12 @@
+from .chains import register_chains_tools
+from .protocols import register_protocols_tools
+from .analytics import register_analytics_tools
+from .operations import register_operations_tools
+from .market import register_market_tools
+
+def register_all_tools(app):
+    register_chains_tools(app)
+    register_protocols_tools(app)
+    register_analytics_tools(app)
+    register_operations_tools(app)
+    register_market_tools(app)
